37659f3363bd9329fb2061ab1b63361531f2f043
matt-williams <matwilliams@hotmail.com>
2013-04-07 17:04:26 +0100

    Don't lock up camera rendering, and also ignore nonsense field of view values.

--- Android AR/src/com/github/matt/williams/android/ar/CameraUtils.java
+++ Android AR/src/com/github/matt/williams/android/ar/CameraUtils.java
@@ -7,7 +7,15 @@ import android.hardware.Camera.CameraInfo;
 import android.hardware.Camera.Parameters;
 import android.hardware.Camera.Size;
 
+import com.github.matt.williams.android.gl.Projection;
+
 public final class CameraUtils {
+    private static final float MINIMUM_HORIZONTAL_VIEW_ANGLE = 10.0f;
+    private static final float MAXIMUM_HORIZONTAL_VIEW_ANGLE = 170.0f;
+    private static final float DEFAULT_HORIZONTAL_VIEW_ANGLE = 60.0f;
+    private static final float MINIMUM_VERTICAL_VIEW_ANGLE = 10.0f;
+    private static final float MAXIMUM_VERTICAL_VIEW_ANGLE = 170.0f;
+    private static final float DEFAULT_VERTICAL_VIEW_ANGLE = 48.0f;
 
     private CameraUtils() {}
 
@@ -35,9 +43,26 @@ public final class CameraUtils {
         camera.setParameters(params);
     }
 
-    public static double getAspectRatio(Camera camera) {
-        Parameters params = camera.getParameters();
-        Size previewSize = params.getPreviewSize();
+    public static double getPixelAspectRatio(Camera camera) {
+        Size previewSize = camera.getParameters().getPreviewSize();
         return (double)previewSize.width / previewSize.height;
+    }
+
+    public static double getViewAngleAspectRatio(Camera camera) {
+        return Math.tan(getSaneHorizontalViewAngle(camera) / 2 * Math.PI / 180) / Math.tan(getSaneVerticalViewAngle(camera) / 2 * Math.PI / 180);
+    }
+
+    public static void setProjection(Projection projection, Camera camera) {
+        projection.setProjection(getSaneHorizontalViewAngle(camera), (float)(getSaneHorizontalViewAngle(camera) / getPixelAspectRatio(camera)));
+    }
+
+    public static float getSaneHorizontalViewAngle(Camera camera) {
+        float horizViewAngle = camera.getParameters().getHorizontalViewAngle();
+        return ((horizViewAngle >= MINIMUM_HORIZONTAL_VIEW_ANGLE) && (horizViewAngle <= MAXIMUM_HORIZONTAL_VIEW_ANGLE)) ? horizViewAngle : DEFAULT_HORIZONTAL_VIEW_ANGLE;
+    }
+
+    public static float getSaneVerticalViewAngle(Camera camera) {
+        float vertViewAngle = camera.getParameters().getVerticalViewAngle();
+        return ((vertViewAngle >= MINIMUM_VERTICAL_VIEW_ANGLE) && (vertViewAngle <= MAXIMUM_VERTICAL_VIEW_ANGLE)) ? vertViewAngle : DEFAULT_VERTICAL_VIEW_ANGLE;
     }
 }
--- Android AR/src/com/github/matt/williams/android/ar/CameraView.java
+++ Android AR/src/com/github/matt/williams/android/ar/CameraView.java
@@ -6,7 +6,6 @@ import javax.microedition.khronos.opengles.GL10;
 import android.content.Context;
 import android.hardware.Camera;
 import android.hardware.Camera.CameraInfo;
-import android.hardware.Camera.Parameters;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.GLSurfaceView.Renderer;
@@ -45,11 +44,10 @@ public class CameraView extends GLSurfaceView implements Renderer {
     public void onResume() {
         super.onResume();
         mCamera = CameraUtils.getCamera(CameraInfo.CAMERA_FACING_BACK);
+        android.util.Log.e("ScreenTarget", "onResume: camera = " + mCamera);
         if (mCamera != null) {
             CameraUtils.setMaxPreviewSize(mCamera);
-            Parameters params = mCamera.getParameters();
-            // TODO: Remove hack for Sony phones that seem to report nonsense viewing angles
-            getProjection().setProjection(params.getHorizontalViewAngle() > 1.0f ? params.getHorizontalViewAngle() : 63.4f, params.getVerticalViewAngle() > 1.0f ? params.getVerticalViewAngle() : 49.6f);
+            CameraUtils.setProjection(mProjection, mCamera);
         }
     }
 
@@ -74,22 +72,27 @@ public class CameraView extends GLSurfaceView implements Renderer {
         Utils.checkErrors("glEnable(GLES20.GL_BLEND)");
         GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
         Utils.checkErrors("glBlendFunc");
-    }
 
-    @Override
-    public void onSurfaceChanged(GL10 gl, int width, int height) {
-        if (mCamera != null) {
-            mScreenTarget.set(width, height, CameraUtils.getAspectRatio(mCamera));
-        }
-    }
-
-    @Override
-    public void onDrawFrame(GL10 gl) {
         GLES20.glDisable(GLES20.GL_DEPTH_TEST);
         Utils.checkErrors("glDisable(GLES20.GL_DEPTH_TEST)");
         GLES20.glDepthMask(false);
         Utils.checkErrors("glDepthMask");
 
+        GLES20.glClearColor(0, 0, 0, 0);
+        Utils.checkErrors("glClearColor");
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        android.util.Log.e("ScreenTarget", "onSurfaceChanged: camera = " + mCamera);
+        if (mCamera != null) {
+            mScreenTarget.set(width, height, CameraUtils.getPixelAspectRatio(mCamera));
+        }
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
         mScreenTarget.renderTo();
         if (mTexture != null) {
             mBillboard.render(getProjection());
